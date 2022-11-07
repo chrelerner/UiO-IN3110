461a0921ec65f1dcf810af8e0c6182e7db1ebdb8
chrisrle <chrisrle@uio.no>
2022-11-07 15:41:45 +0100

    Cleaned up code and fixed bugs.

--- assignment4/filter_urls.py
+++ assignment4/filter_urls.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urljoin
+from requesting_urls import get_html
 
 ## -- Task 2 -- ##
 
@@ -17,29 +18,31 @@ def find_urls(
     """
 
     # Finds all the URLs or URL paths without #-fragments.
-    hrefs = find_a_href(html)
+    raw_urls = find_urls_inner(html)
     urls = set()
     
-    for href in hrefs:
+    for url in raw_urls:
         # Combines base URL with URL path.
-        if re.search(r'^/[^/]', href): # If href begins with one /
-            full_url = f"{base_url}{href}"
+        if re.search(r'^/[^/]', url): # If href begins with one /
+            full_url = f"{base_url}{url}"
             urls.add(full_url)
             
         # Adds https protocol to the URL.
-        elif re.search(r'^//', href): # If href begins with //
-            full_url = f"https:{href}"
+        elif re.search(r'^//', url): # If href begins with //
+            full_url = f"https:{url}"
             urls.add(full_url)
             
+        # Already complete url.
         else:
-            urls.add(href)
+            urls.add(url)
         
     # Writes to file if requested
     if output:
         print(f"Writing to: {output}")
-        output_file = open(output, "w")
+        output_file = open(output, "w", encoding="utf-8")
+        output_file.write("Found URLs:\n")
         for url in urls:
-            output_file.write(f"\n{url}")
+            output_file.write(f"\n- {url}\n")
         
     return urls
 
@@ -52,26 +55,32 @@ def find_articles(html: str, output=None) -> set:
         - (set) : a set with urls to all the articles found
     """
     
+    # Finds the URLs and defines patterns to search with.
     urls = find_urls(html)
-    pattern = r'https://[a-z.]*wikipedia.org/wiki/[^:]+\b' # Regex pattern to detect wikipedia URLs without :
+    pattern_1 = r'https://[a-z.]*wikipedia.org/wiki/(.+)' # Regex pattern to detect wikipedia URLs
+    pattern_2 = r':' # Regex pattern to filer out URLs with ':' at the end.
     articles = set()
     
     for url in urls:
-        if re.search(pattern, url):
-            articles.add(url)
+        match_1 = re.search(pattern_1, url)
+        if match_1:
+            match_2 = re.search(pattern_2, match_1.group(1))
+            if not match_2:
+                articles.add(url)
 
     # Writes to file if wanted
     if output:
         print(f"Writing to: {output}")
-        output_file = open(output, "w")
+        output_file = open(output, "w", encoding="utf-8")
+        output_file.write("Found articles:\n")
         for article in articles:
-            output_file.write(f"\n{article}")
+            output_file.write(f"\n- {article}\n")
             
     return articles
     
 
 # Function derived from find_img_src
-def find_a_href (html: str):
+def find_urls_inner (html: str):
     """Find all href attributes of a tags in an HTML string
     
     Filters out fragments of the URLs. If the href attribute
@@ -90,16 +99,16 @@ def find_a_href (html: str):
     a_pat = re.compile(r"<a[^>]+>", flags=re.IGNORECASE)
     
     # This pattern finds the text between quotes of the `href` attribute
-    href_pat = re.compile(r'href="([^"#]+)("|#)', flags=re.IGNORECASE)
-    href_set = set()
+    href_pat = re.compile(r'href="([^"#]+)(?:"|#)', flags=re.IGNORECASE)
+    raw_urls = set()
     
     # Checks all the 'a' tags for valid href sections.
     for a_tag in a_pat.findall(html):
         match = href_pat.search(a_tag)
         if match:
-            href_set.add(match.group(1))
+            raw_urls.add(match.group(1))
             
-    return href_set
+    return raw_urls
     
 
 
@@ -129,25 +138,23 @@ def find_img_src(html: str):
             src_set.add(match.group(1))
     return src_set
 
+
 if __name__ == "__main__":
     
-    html = """
-    <a href="#fragment-only">anchor link</a>
-    <a id="some-id" href="/relative/path#fragment">relative link</a>
-    <a href="//other.host/same-protocol">same-protocol link</a>
-    <a href="https://example.com">absolute URL</a>
-    """
+    #html = """
+    #<a href="#fragment-only">anchor link</a>
+    #<a id="some-id" href="/relative/path#fragment">relative link</a>
+    #<a href="//other.host/same-protocol">same-protocol link</a>
+    #<a href="https://example.com">absolute URL</a>
+    #"""
     
-    hrefs = find_a_href(html)
+    html = get_html(url="https://en.wikipedia.org/wiki/Nobel_Prize")
     
-    for i in hrefs:
-        print(i)
+    find_urls(html=html, output="out_filter_urls_find_urls.txt")
     
-    print("\n")
-    urls = find_urls(html)
+    find_articles(html=html, output="out_filter_urls_find_articles.txt")
     
-    for i in urls:
-        print(i)
+    
         
         
     
